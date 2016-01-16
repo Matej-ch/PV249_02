f33dbded234e1c86467fd9cab3a610e3d432d937
Matej-ch <matejchalachan@gmail.com>
2016-01-16 20:20:07 +0100

    complaying to rubocop

--- app.rb
+++ app.rb
@@ -11,39 +11,38 @@ UNKNOWN            = 'neznámy'
 results            = []
 
 ARGV.each do |url|
-   uri = URI(url)
-   if uri.host
-     req = Net::HTTP.new(uri.host, uri.port)
-
-     if uri.path.to_s.strip.length == 0
-       query_updated = QUERY.gsub(/#query#/,url)
-       content = Net::HTTP.get(URI (FACEBOOK_API_URL + query_updated))
-       json_hash = JSON.parse(content)
-       results << {
-           'host'                 => uri.host, # base domain,
-           'url'                  => url,
-           'Počet likes článku'   => json_hash[0]['like_count'], # likes_count#
-           'Počet sdílení článku' => json_hash[0]['share_count'] # shares_count#
-       }
-     else
-       res = req.request_head(uri.path)
-        if res.code != '202'
-          results << {
-              'host'                 => UNKNOWN, # base domain,
-              'url'                  => url,
-              'Počet likes článku'   => UNKNOWN, # likes_count#
-              'Počet sdílení článku' => UNKNOWN # shares_count#
-          }
-        end
-     end
-   else
-     results << {
-         'host'                 => UNKNOWN, # base domain,
-         'url'                  => url,
-         'Počet likes článku'   => UNKNOWN, # likes_count#
-         'Počet sdílení článku' => UNKNOWN # shares_count#
-     }
-   end
+  uri = URI(url)
+  if uri.host
+    req = Net::HTTP.new(uri.host, uri.port)
+    if uri.path.to_s.strip.length == 0
+      query_updated = QUERY.gsub(/#query#/, url)
+      content = Net::HTTP.get(URI(FACEBOOK_API_URL + query_updated))
+      json_hash = JSON.parse(content)
+      results << {
+        'host' => uri.host, # base domain,
+        'url' => url,
+        'Počet likes článku' => json_hash[0]['like_count'], # likes_count#
+        'Počet sdílení článku' => json_hash[0]['share_count'] # shares_count#
+      }
+    else
+      res = req.request_head(uri.path)
+      if res.code != '202'
+        results << {
+          'host' => UNKNOWN, # base domain,
+          'url' => url,
+          'Počet likes článku' => UNKNOWN, # likes_count#
+          'Počet sdílení článku' => UNKNOWN # shares_count#
+        }
+      end
+    end
+  else
+    results << {
+      'host' => UNKNOWN, # base domain,
+      'url' => url,
+      'Počet likes článku' => UNKNOWN, # likes_count#
+      'Počet sdílení článku' => UNKNOWN # shares_count#
+    }
+  end
 end
 
 pp results
